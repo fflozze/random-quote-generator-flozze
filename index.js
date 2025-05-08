f4b052bd50cc97263a41ea1b5b632dfd900529a2
flozze <flozze@localhost.localdomain>
2025-05-08 19:01:45 +0200

    ajout js fonctionel reste a géré css

---
+++ index.js
@@ -0,0 +1,58 @@
+const citations = [
+  {
+    texte: "Le plus grand risque est de ne prendre aucun risque.",
+    auteur: "Mark Zuckerberg",
+  },
+  {
+    texte: "Ils ne savaient pas que c'était impossible, alors ils l'ont fait.",
+    auteur: "Mark Twain",
+  },
+  {
+    texte:
+      "La vie, c’est comme une bicyclette, il faut avancer pour ne pas perdre l’équilibre.",
+    auteur: "Albert Einstein",
+  },
+  {
+    texte: "Le succès, c’est tomber sept fois, se relever huit.",
+    auteur: "Proverbe japonais",
+  },
+  {
+    texte: "Fais de ta vie un rêve, et d’un rêve, une réalité.",
+    auteur: "Antoine de Saint-Exupéry",
+  },
+  {
+    texte: "L’imagination est plus importante que le savoir.",
+    auteur: "Albert Einstein",
+  },
+  {
+    texte:
+      "Commencez par changer en vous ce que vous voulez changer autour de vous.",
+    auteur: "Gandhi",
+  },
+  {
+    texte: "L’action est la clé fondamentale de tout succès.",
+    auteur: "Pablo Picasso",
+  },
+  {
+    texte:
+      "Ce n’est pas parce que les choses sont difficiles que nous n’osons pas, c’est parce que nous n’osons pas qu’elles sont difficiles.",
+    auteur: "Sénèque",
+  },
+  {
+    texte: "Rêve grand, commence petit. Mais surtout, commence.",
+    auteur: "Simon Sinek",
+  },
+];
+
+function write() {
+  let randomIndex = Math.floor(Math.random() * citations.length);
+  const citationsRandom = citations[randomIndex];
+
+  let text = document.getElementById("text");
+  text.innerText = citationsRandom.texte;
+
+  let autor = document.getElementById("autor");
+  autor.innerText = citationsRandom.auteur;
+}
+
+document.getElementById("newCitation").addEventListener("click", write);
